07e6b0fb4e3d8e1030a94cdc412aa078f1fe4f6a
Eryk <eryk@ZipcodersMBP71.fios-router.home>
2022-11-27 22:10:42 -0500

    add copy and remove

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -37,7 +37,7 @@ public class SinglyLinkedList<T extends Comparable> {
         T value;
         Node<T> nextNode;
 
-        public Node(T value, Node<T> nextValue) {
+        public Node(T value) {
             this.value = value;
             this.nextNode = null; //set default to null
         }
@@ -58,7 +58,8 @@ public class SinglyLinkedList<T extends Comparable> {
             this.nextNode = nextNode;
         }
     }
-    public void add(Node<T> node){
+    public void add(T valueToAdd){
+        Node<T> node = new Node<T>(valueToAdd);
         if(getHead() == null) setHead(node);
         else {
             this.tail.setNextNode(node);
@@ -94,6 +95,38 @@ public class SinglyLinkedList<T extends Comparable> {
             }
         }
         return -1;
+    }
+    public int size(){return getSize();}
+
+    public SinglyLinkedList<T> copy(){
+        SinglyLinkedList<T> theCopy = new SinglyLinkedList<T>();
+        Node<T> currentNode = this.head;
+        while(currentNode !=  null ){
+            theCopy.add(currentNode.getValue());
+            currentNode =  currentNode.getNextNode();
+        }
+        return theCopy;
+    }
+    public void remove(int index){
+        T valueToRemove = get(index);
+        if(index == 0) this.setHead(head.nextNode); // 0 index case
+        else {
+            Node<T> currentNode = this.head;
+            //iterate to the one before index
+            while (valueToRemove != currentNode.getNextNode().getValue()) {
+                currentNode = currentNode.getNextNode();
+            }
+            //last index case
+            if(currentNode.getNextNode().getNextNode() == null){
+                currentNode.setNextNode(null);
+                setTail(currentNode);
+            }
+            //change node before to point to the next one after the next
+            else {
+                currentNode.setNextNode(currentNode.getNextNode().getNextNode());
+            }
+        }
+        setSize(size--);
     }
 
 }
